df60bdb9f35f37326903ec1236f55210f36f1aee
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-07-18 18:22:03 +0400

    update

--- src/app/movies-error/page.tsx
+++ src/app/movies-error/page.tsx
@@ -36,7 +36,7 @@ export default function MoviesError() {
         <div className="p-20 w-full">
           <div className="grid flow-column w-full justify-center items-center">
             <h1 className="text-lg font-bold">
-              Can't get movies list 😭 {isError} 😭
+              Can't get movies list {isError}
             </h1>
             <Link
               href="/"
